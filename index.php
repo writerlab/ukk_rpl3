ec0ce8b9aecd1ac779ec260321500dbc2bfdbcb6
writerlab <netspytux@gmail.com>
2019-01-24 15:15:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include('konten/header.html'); ?>
+
+<div id="wrapper">
+    
+    <?php include('konten/nav.html'); ?>
+
+    <div id="page-wrapper">
+        
+    </div>
+</div>
+
+<?php include('konten/footer.html'); ?>
